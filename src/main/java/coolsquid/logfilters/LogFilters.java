e4b2cb9907e0941246521320aac83e94a2b1eeb5
coolsquid <9624459+coolsquid@users.noreply.github.com>
2019-05-16 14:44:33 +0200

    Updated the version number to 1.0.2.

--- src/main/java/coolsquid/logfilters/LogFilters.java
+++ src/main/java/coolsquid/logfilters/LogFilters.java
@@ -21,7 +21,7 @@ public class LogFilters implements IFMLLoadingPlugin, IFMLCallHook {
 
 	public static final String MODID = "logfilters";
 	public static final String NAME = "LogFilters";
-	public static final String VERSION = "1.0.1";
+	public static final String VERSION = "1.0.2";
 	public static final String DEPENDENCIES = "required-after:forge@[14.21.1.2387,)";
 	public static final String UPDATE_JSON = "https://gist.githubusercontent.com/coolsquid/39d310b4b62141761164e854d1d452da/raw/logfilters.json";
 
